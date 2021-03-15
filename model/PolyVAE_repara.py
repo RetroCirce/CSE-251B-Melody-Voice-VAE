a07f50daa3736ea7d3945dc9bb19a8c437843e0c
ZexueHe <iameuxzh@gmail.com>
2021-03-14 23:23:52 -0700

    fix bug in 320 length

--- model/PolyVAE_repara.py
+++ model/PolyVAE_repara.py
@@ -9,9 +9,9 @@ import numpy as np
 
 class PolyVAE_repara(nn.Module):
     def __init__(
-        self, input_dims = 130, 
+        self, input_dims = 90, 
         hidden_dims = 512, z_dims = 1024, 
-        seq_len = 160, beat_num = 10, tick_num = 16, 
+        seq_len = 320, beat_num = 20, tick_num = 16, 
         decay = 1000):
         super(PolyVAE_repara, self).__init__()
         # encoder
@@ -70,15 +70,15 @@ class PolyVAE_repara(nn.Module):
         self.decay = torch.FloatTensor([decay])
         
     def encoder(self, rx):
-        rx = self.embedding(rx)
+        rx = self.embedding(rx) #rx: [64, 160]
         rx = self.encoder_gru(rx)[-1]
         rx = rx.transpose(0,1).contiguous()
         rx = rx.view(rx.size(0), -1)
         r_mu = self.linear_mu(rx)
         # r_var = self.linear_var(rx)
         r_log_var = self.linear_log_var(rx)
-        r_var = torch.exp(r_log_var)
-        std_z = torch.randn(r_var.size()).cuda()
+        r_var = torch.exp(r_log_var)#[64, 1024]
+        std_z = torch.randn(r_var.size()).cuda() #[64, 1024]
         assert not std_z.requires_grad
         #r_dis = Normal(r_mu, r_var)
         self.z_mu=r_mu
@@ -88,42 +88,42 @@ class PolyVAE_repara(nn.Module):
         return r_mu + r_var * std_z
 
     def final_decoder(self, z, gd, is_train = True):
-        gd = self.d_embedding(gd)
+        gd = self.d_embedding(gd) #[64, 160, 10]
         beat_out = self.forward_beat(z)
         recon = self.forward_tick(beat_out, gd, is_train)
         return recon
 
     def forward_beat(self, z):
         batch_size = z.size(0)
-        h_beat = self.z_to_beat_hidden(z)
+        h_beat = self.z_to_beat_hidden(z) #[64, 1024]
         h_beat = h_beat.view(batch_size, self.beat_layer_num, -1)
-        h_beat = h_beat.transpose(0,1).contiguous()
+        h_beat = h_beat.transpose(0,1).contiguous() #[2, 64, 512]
         beat_input = self.beat_0.unsqueeze(0).expand(
             batch_size, self.beat_num, 1
-        )
-        beat_out, _ = self.beat_gru(beat_input, h_beat)
+        ) #[64, 10, 1]
+        beat_out, _ = self.beat_gru(beat_input, h_beat) #[64, 10, 512]
 #         print("beat_out",beat_out.size())
         return beat_out
 
     def forward_tick(self, beat_out, gd, is_train = True):
         ys = []
-        batch_size = beat_out.size(0)
-        tick_input = self.tick_0.unsqueeze(0).expand(batch_size, self.vocab_dims)
-        tick_input = tick_input.unsqueeze(1)
+        batch_size = beat_out.size(0) #tick_0 [10]
+        tick_input = self.tick_0.unsqueeze(0).expand(batch_size, self.vocab_dims)#[64, 10]
+        tick_input = tick_input.unsqueeze(1) #[64,1,10]
         y = tick_input
 #         print(beat_out)
         for i in range(self.beat_num):
-            h_tick = self.beat_to_tick_hidden(beat_out[:, i, :])
+            h_tick = self.beat_to_tick_hidden(beat_out[:, i, :]) #[64, 1024]
             h_tick = h_tick.view(batch_size, self.tick_layer_num, -1)
-            h_tick = h_tick.transpose(0,1).contiguous()
+            h_tick = h_tick.transpose(0,1).contiguous() #[2,64, 512]
 #             print(h_tick)
-            c_tick = self.beat_to_tick_input(beat_out[:, i, :]).unsqueeze(1)
-            for j in range(self.tick_num):
+            c_tick = self.beat_to_tick_input(beat_out[:, i, :]).unsqueeze(1) #[64,1, 512]
+            for j in range(self.tick_num): # tick_num=16
                 y = torch.cat((y, c_tick), -1)
 #                 print("y size:",y.size())
                 y, h_tick = self.tick_gru(y, h_tick)
-                y = y.contiguous().view(y.size(0), -1)
-                y = self.tick_to_note(y)
+                y = y.contiguous().view(y.size(0), -1) #[64, 512]
+                y = self.tick_to_note(y) #[64, 130]
 #                 print("after embed:", y.size())
                 ys.append(y)
                 y = y.argmax(-1)
@@ -151,7 +151,7 @@ class PolyVAE_repara(nn.Module):
             self.iteration += 1
         # r_dis = self.encoder(x)
         # z = r_dis.rsample()
-        z = self.encoder(x)
+        z = self.encoder(x) # [64, 1024]
         recon = self.final_decoder(z, gd)
         # return recon, r_dis, self.iteration
         return recon, z, self.iteration, self.z_mu, self.z_var
---
+++ train_repara.py
@@ -0,0 +1,253 @@
+import torch
+import os
+import numpy as np
+from torch import optim
+from torch.distributions import kl_divergence, Normal
+from torch.nn import functional as F
+from torch.optim.lr_scheduler import ExponentialLR
+from model import PolyVAE_repara
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+
+class MinExponentialLR(ExponentialLR):
+    def __init__(self, optimizer, gamma, minimum, last_epoch=-1):
+        self.min = minimum
+        super(MinExponentialLR, self).__init__(optimizer, gamma, last_epoch=-1)
+
+    def get_lr(self):
+        return [
+            max(base_lr * self.gamma**self.last_epoch, self.min)
+            for base_lr in self.base_lrs
+        ]
+###############################
+# initial parameters
+s_dir = "/home/zexue/Downloads/CSE251B/CSE-251B-Melody-Voice-VAE/"
+batch_size = 32
+n_epochs = 200
+data_path = [s_dir + "data/poly_train_fix.npy",
+             s_dir + "data/poly_validate_fix.npy",
+             s_dir + "data/poly_test_fix.npy"]
+save_path = ""
+lr = 1e-4
+decay = 0.9999
+hidden_dims = 512
+z_dims = 1024
+vae_beta = 0.1
+input_dims = 90
+seq_len = 20 * 16
+beat_num = 20
+tick_num = 16
+save_period = 10
+##############################
+train_set = np.load(data_path[0], allow_pickle = True)
+validate_set = np.load(data_path[1],allow_pickle = True) 
+test_set = np.load(data_path[2],allow_pickle = True) 
+
+train_x = []
+for i,data in enumerate(train_set):
+    temp = []
+    for d in data["layers"]:
+        temp += d
+    train_x.append(temp)
+train_x = np.array(train_x)
+# print(train_x.shape)
+
+validate_x = []
+for i,data in enumerate(validate_set):
+    temp = []
+    for d in data["layers"]:
+        temp += d
+    validate_x.append(temp)
+validate_x = np.array(validate_x)
+# print(train_x.shape)
+
+test_x = []
+for i,data in enumerate(test_set):
+    temp = []
+    for d in data["layers"]:
+        temp += d
+    test_x.append(temp)
+test_x = np.array(test_x)
+
+
+train_x = torch.from_numpy(train_x).long()
+validate_x = torch.from_numpy(validate_x).long()
+test_x = torch.from_numpy(test_x).long()
+
+print(train_x.size())
+print(validate_x.size())
+print(test_x.size())
+
+train_set = TensorDataset(train_x)
+validate_set = TensorDataset(validate_x)
+
+train_set = DataLoader(
+    dataset = train_set,
+    batch_size = batch_size, 
+    shuffle = True, 
+    num_workers = 0, 
+    pin_memory = True, 
+    drop_last = True
+)
+validate_set = DataLoader(
+    dataset = validate_set,
+    batch_size = batch_size, 
+    shuffle = False, 
+    num_workers = 0, 
+    pin_memory = True, 
+    drop_last = True
+)
+model = PolyVAE_repara(input_dims, hidden_dims, z_dims, seq_len, beat_num, tick_num, 4000)
+optimizer = optim.Adam(model.parameters(), lr = lr)
+if decay > 0:
+    scheduler = MinExponentialLR(optimizer, gamma = decay, minimum = 1e-5)
+if torch.cuda.is_available():
+    print('Using: ', torch.cuda.get_device_name(torch.cuda.current_device()))
+    model.cuda()
+else:
+    print('Using: CPU')
+
+validate_data = []
+for i,d in enumerate(validate_set):
+    validate_data.append(d[0])
+print(len(validate_data))
+
+def loss_function(recon, target, z_mean, z_std, beta):
+    CE = F.cross_entropy(recon.view(-1, recon.size(-1)), target, reduction = "mean")
+#     rhy_CE = F.nll_loss(recon_rhythm.view(-1, recon_rhythm.size(-1)), target_rhythm, reduction = "mean")
+    
+    # normal1 =  std_normal(r_dis.mean.size())
+    # KLD1 = kl_divergence(r_dis, normal1).mean()
+    mean_sq=z_mean * z_mean
+    std_sq=z_std * z_std
+    KLD1 = 0.5 * torch.mean(mean_sq + std_sq - torch.log(std_sq) - 1)
+
+    max_indices = recon.view(-1, recon.size(-1)).max(-1)[-1]
+#     print(max_indices)
+    correct = max_indices == target
+    acc = torch.sum(correct.float()) / target.size(0)
+    return acc, CE + beta * (KLD1)
+
+device = torch.device(torch.cuda.current_device())
+
+logs = []
+iteration = 0
+step = 0
+best_acc=0
+for epoch in range(n_epochs):
+    print("epoch: %d\n__________________________________________" % (epoch), flush = True)
+    mean_loss = 0.0
+    mean_acc = 0.0
+    v_mean_loss = 0.0
+    v_mean_acc = 0.0
+    total = 0
+    for i, d in enumerate(train_set):
+        # validate display
+        x = gd = d[0]
+        model.train()
+        j = i % len(validate_data)
+        v_x = v_gd = validate_data[j]
+        
+        x = x.to(device = device,non_blocking = True)
+        gd = gd.to(device = device,non_blocking = True)
+        v_x = v_x.to(device = device,non_blocking = True)
+        v_gd = v_gd.to(device = device,non_blocking = True)
+            
+        optimizer.zero_grad()
+        recon, r_dis, iteration, z_mu, z_var = model(x, gd)
+        
+        acc, loss = loss_function(recon, gd.view(-1), z_mu, z_var, vae_beta)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+        optimizer.step()
+        mean_loss += loss.item()
+        mean_acc += acc.item()
+        
+        model.eval()
+        with torch.no_grad():
+            v_recon, v_r_dis, _, v_z_mu, v_z_var = model(v_x, v_gd)
+            v_acc, v_loss = loss_function(v_recon, v_gd.view(-1), v_z_mu, v_z_var, vae_beta)
+            v_mean_loss += v_loss.item()
+            v_mean_acc += v_acc.item()
+        step += 1
+        total += 1
+        if decay > 0:
+            scheduler.step()
+        if i % 400 == 0:
+            print("batch %d loss: %.5f acc: %.5f | val loss %.5f acc: %.5f iteration: %d"  
+                % (i,loss.item(), acc.item(), v_loss.item(),v_acc.item(),iteration),flush = True)
+    mean_loss /= total
+    mean_acc /= total
+    v_mean_loss /= total
+    v_mean_acc /= total
+    print("epoch %d loss: %.5f acc: %.5f | val loss %.5f acc: %.5f iteration: %d"  
+              % (epoch, mean_loss, mean_acc, v_mean_loss, v_mean_acc, iteration),flush = True)
+    logs.append([mean_loss,mean_acc,v_mean_loss,v_mean_acc,iteration])
+    if v_mean_acc > best_acc:
+        filename = "best-" + 'loss_' + str(mean_loss) + "_" + str(epoch+1) + "_" + str(iteration) + ".pt"
+        torch.save(model.cpu().state_dict(),save_path + filename)
+        best_acc=v_mean_acc
+        model.cuda()
+np.save("320-log.npy", logs)
+        
+
+
+# test
+model.eval()
+device = torch.device(torch.cuda.current_device())
+mean_acc = 0.0
+mean_loss=0.0
+ys = []
+gds = []
+output = []
+for i, d in enumerate(validate_set):
+    # validate display
+    x = gd = d[0]
+        
+    x = x.to(device = device,non_blocking = True)
+    gd = gd.to(device = device,non_blocking = True)
+            
+    recon, r_dis, iteration, z_mu, z_var = model(x, gd)
+        
+    acc,v_loss = loss_function(recon, gd.view(-1), z_mu, z_var, vae_beta)
+    mean_acc += acc.item()
+    mean_loss+=v_loss.item()
+#     z = r_dis.rsample()
+    pred = recon.argmax(-1)
+    output.append({"gd":x.cpu().detach().numpy(), "pred":pred.cpu().detach().numpy(), "acc": acc.item()})
+#     print(pred.size())
+#     print(v_gd.size())
+#     output.append([pred.cpu().detach().numpy(), v_gd.cpu().detach().numpy()])
+#     print(v_loss.item())
+
+    # np.save("dismeasurevae_gen_exp.npy", output)
+print("******validation acc: ", mean_acc/(i+1), "validation loss: ", mean_loss/(i+1))
+
+model.eval()
+device = torch.device(torch.cuda.current_device())
+mean_acc = 0.0
+mean_loss=0.0
+ys = []
+gds = []
+output = []
+for i, d in enumerate(test_set):
+    # validate display
+    x = gd = d[0]
+        
+    x = x.to(device = device,non_blocking = True)
+    gd = gd.to(device = device,non_blocking = True)
+            
+    recon, r_dis, iteration, z_mu, z_var = model(x, gd)
+        
+    acc,v_loss = loss_function(recon, gd.view(-1), z_mu, z_var, vae_beta)
+    mean_acc += acc.item()
+    mean_loss+=v_loss.item()
+#     z = r_dis.rsample()
+    pred = recon.argmax(-1)
+    output.append({"gd":x.cpu().detach().numpy(), "pred":pred.cpu().detach().numpy(), "acc": acc.item()})
+#     print(pred.size())
+#     print(v_gd.size())
+#     output.append([pred.cpu().detach().numpy(), v_gd.cpu().detach().numpy()])
+#     print(v_loss.item())
+
+    # np.save("dismeasurevae_gen_exp.npy", output)
+print("******test acc: ", mean_acc/(i+1), "validation loss: ", mean_loss/(i+1))
