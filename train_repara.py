b1d10d8b68da0d9cf77752ff544a237e56bdc562
ZexueHe <iameuxzh@gmail.com>
2021-03-15 17:07:01 -0700

    plot figure in train

--- train_repara.py
+++ train_repara.py
@@ -21,13 +21,18 @@ class MinExponentialLR(ExponentialLR):
 ###############################
 # initial parameters
 s_dir = "/home/zexue/Downloads/CSE251B/CSE-251B-Melody-Voice-VAE/"
-batch_size = 256
+batch_size = 64
 n_epochs = 200
 data_path = [s_dir + "data/poly_train_fix.npy",
              s_dir + "data/poly_validate_fix.npy",
              s_dir + "data/poly_test_fix.npy"]
+##############################
+train_set = np.load(data_path[0], allow_pickle = True)
+validate_set = np.load(data_path[1],allow_pickle = True) 
+test_set = np.load(data_path[2],allow_pickle = True)
+
 save_path = ""
-lr = 1e-3
+lr = 1e-4
 decay = 0.9999
 hidden_dims = 512
 z_dims = 1024
@@ -37,10 +42,7 @@ seq_len = 20 * 16
 beat_num = 20
 tick_num = 16
 save_period = 10
-##############################
-train_set = np.load(data_path[0], allow_pickle = True)
-validate_set = np.load(data_path[1],allow_pickle = True) 
-test_set = np.load(data_path[2],allow_pickle = True) 
+
 
 train_x = []
 for i,data in enumerate(train_set):
@@ -93,7 +95,7 @@ validate_set = DataLoader(
     dataset = validate_set,
     batch_size = batch_size, 
     shuffle = False, 
-    num_workers = 0, 
+    num_workers = 8, 
     pin_memory = True, 
     drop_last = True
 )
@@ -102,7 +104,7 @@ test_set = DataLoader(
     dataset = test_set,
     batch_size = batch_size, 
     shuffle = False, 
-    num_workers = 0, 
+    num_workers = 8, 
     pin_memory = True, 
     drop_last = True
 )
@@ -120,6 +122,8 @@ validate_data = []
 for i,d in enumerate(validate_set):
     validate_data.append(d[0])
 print(len(validate_data))
+experiment_dir = os.path.join("experiment_data/", experiment_name)
+os.makedirs(experiment_dir, exist_ok=True)
 
 def loss_function(recon, target, z_mean, z_std, beta):
     CE = F.cross_entropy(recon.view(-1, recon.size(-1)), target, reduction = "mean")
@@ -136,6 +140,54 @@ def loss_function(recon, target, z_mean, z_std, beta):
     correct = max_indices == target
     acc = torch.sum(correct.float()) / target.size(0)
     return acc, CE + beta * (KLD1)
+
+def save_model(model, optimizer):
+    root_model_path = os.path.join(experiment_dir, 'best_model.pt')
+    model_dict = model.state_dict()
+    state_dict = {'model': model_dict, 'optimizer': optimizer.state_dict()}
+    torch.save(state_dict, root_model_path)
+
+
+def load_model(model, optimizer):
+    state_dict = torch.load(os.path.join(experiment_dir, 'best_model.pt'))
+    model.load_state_dict(state_dict['model'])
+    optimizer.load_state_dict(state_dict['optimizer'])
+
+
+def record_stats(train_loss, train_acc, val_loss, val_acc):
+    training_losses.append(train_loss)
+    training_accs.append(train_acc)
+    val_losses.append(val_loss)
+    val_accs.append(val_acc)
+
+    plot_stats()
+
+
+def plot_stats():
+    e = len(training_losses)
+    x_axis = np.arange(1, e + 1, 1)
+    plt.figure()
+    plt.plot(x_axis, training_losses, label="Training Loss")
+    plt.plot(x_axis, val_losses, label="Validation Loss")
+    plt.xlabel("Epochs")
+    plt.legend(loc='best')
+    plt.savefig(os.path.join(experiment_dir, "loss_plot.png"))
+    plt.close()
+
+    plt.figure()
+    plt.plot(x_axis, training_accs, label="Training Accuracy")
+    plt.plot(x_axis, val_accs, label="Validation Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend(loc='best')
+    plt.savefig(os.path.join(experiment_dir, "acc_plot.png"))
+    plt.close()
+
+
+training_losses = []
+val_losses = []
+training_accs = []
+val_accs = []
+best_loss = 1000
 
 device = torch.device(torch.cuda.current_device())
 
@@ -182,26 +234,27 @@ for epoch in range(n_epochs):
         total += 1
         if decay > 0:
             scheduler.step()
-        if i % 400 == 0:
+        if i % 200 == 0:
             print("batch %d loss: %.5f acc: %.5f | val loss %.5f acc: %.5f iteration: %d"  
                 % (i,loss.item(), acc.item(), v_loss.item(),v_acc.item(),iteration),flush = True)
     mean_loss /= total
     mean_acc /= total
     v_mean_loss /= total
     v_mean_acc /= total
+    record_stats(mean_loss, mean_acc, v_mean_loss, v_mean_acc)
+
     print("epoch %d loss: %.5f acc: %.5f | val loss %.5f acc: %.5f iteration: %d"  
               % (epoch, mean_loss, mean_acc, v_mean_loss, v_mean_acc, iteration),flush = True)
     logs.append([mean_loss,mean_acc,v_mean_loss,v_mean_acc,iteration])
-    if v_mean_acc > best_acc:
-        filename = "best-" + 'loss_' + str(mean_loss) + "_" + str(epoch+1) + "_" + str(iteration) + ".pt"
-        torch.save(model.cpu().state_dict(),save_path + filename)
-        best_acc=v_mean_acc
+    if v_mean_loss < best_loss:
+        save_model(model, optimizer)
+        best_loss=v_mean_loss
         model.cuda()
-np.save("320-log.npy", logs)
         
 
 
 # test
+model=load_model(model, optimizer)
 model.eval()
 device = torch.device(torch.cuda.current_device())
 mean_acc = 0.0
@@ -224,12 +277,6 @@ for i, d in enumerate(validate_set):
 #     z = r_dis.rsample()
     pred = recon.argmax(-1)
     output.append({"gd":x.cpu().detach().numpy(), "pred":pred.cpu().detach().numpy(), "acc": acc.item()})
-#     print(pred.size())
-#     print(v_gd.size())
-#     output.append([pred.cpu().detach().numpy(), v_gd.cpu().detach().numpy()])
-#     print(v_loss.item())
-
-    # np.save("dismeasurevae_gen_exp.npy", output)
 print("******validation acc: ", mean_acc/(i+1), "validation loss: ", mean_loss/(i+1))
 
 model.eval()
@@ -254,10 +301,4 @@ for i, d in enumerate(test_set):
 #     z = r_dis.rsample()
     pred = recon.argmax(-1)
     output.append({"gd":x.cpu().detach().numpy(), "pred":pred.cpu().detach().numpy(), "acc": acc.item()})
-#     print(pred.size())
-#     print(v_gd.size())
-#     output.append([pred.cpu().detach().numpy(), v_gd.cpu().detach().numpy()])
-#     print(v_loss.item())
-
-    # np.save("dismeasurevae_gen_exp.npy", output)
 print("******test acc: ", mean_acc/(i+1), "validation loss: ", mean_loss/(i+1))
